1b77de1528edb5264bb5adfd1703a73985e77226
Apple <apple@Dineths-MacBook-Pro.local>
2022-10-28 22:37:48 +0530

    dio interceptors added

--- lib/core/interceptors/dio_interceptor.dart
+++ lib/core/interceptors/dio_interceptor.dart
@@ -1,21 +1,29 @@
 import 'package:dio/dio.dart';
+import 'package:flutter/material.dart';
 
-class DioInterceptor extends InterceptorsWrapper{
+class DioInterceptor extends InterceptorsWrapper {
   @override
   void onRequest(RequestOptions options, RequestInterceptorHandler handler) {
-    // TODO: implement onRequest
+    debugPrint('onRequest ${options.uri}');
     super.onRequest(options, handler);
   }
 
   @override
   void onError(DioError err, ErrorInterceptorHandler handler) {
-    // TODO: implement onError
+    if (err.type == DioErrorType.response) {
+      debugPrint('------------------------');
+      debugPrint(err.error);
+      debugPrint(err.response?.realUri.toString());
+      debugPrint(err.response?.statusCode.toString());
+      debugPrint(err.response?.data.toString());
+      debugPrint('------------------------');
+    }
     super.onError(err, handler);
   }
 
   @override
   void onResponse(Response response, ResponseInterceptorHandler handler) {
-    // TODO: implement onResponse
+    debugPrint('onResponse ${response.statusCode} ${response.realUri}');
     super.onResponse(response, handler);
   }
 }
